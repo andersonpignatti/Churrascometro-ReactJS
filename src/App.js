34134987e1140f700b18fa56b36c95d6a16c5688
Anderson Pignatti <andersonpignatti@outlook.com>
2022-07-02 17:15:30 -0300

    master

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <p>Adultos<input type="text" id="adultos" placeholder="Quantidade de adultos"></input></p>
       <p>Crianças<input type="text" id="crianças" placeholder="Quantidade de crianças"></input></p>
       <p>Duração<input type="tel" id="duração" placeholder="Duração do evento em horas"></input></p>
-      <button id="botao" onclick={'enviar'}>Calcular</button>
+      <button id="botao" onclick={'enviar'}>CALCULAR</button>
       </header>
     </div>
     
